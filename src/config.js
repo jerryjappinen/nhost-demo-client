7901ddf1905bb1f6620e96dcc55adf3cc4798295
Jerry Jäppinen <eiskis@gmail.com>
2022-03-14 12:29:44 +0200

    Default URL can be written over

--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
 export const baseUrl = process.env.BASE_URL
-export const nhostBackendUrl = process.env.VUE_APP_NHOST_BACKEND_URL
+
+
+
+// The fallback is the URL to Nhost's demo backend
+// Create an .env file or set an environment variable to override this
+export const nhostBackendUrl = process.env.VUE_APP_NHOST_BACKEND_URL || 'https://iyoswngikqxwwvqqdaom.nhost.run'
