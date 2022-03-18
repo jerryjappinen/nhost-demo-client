c14351904e8a1da1d2d5f04b359a2a9c65a860c9
Jerry Jäppinen <eiskis@gmail.com>
2022-03-18 18:12:33 +0200

    Simplified trigger

--- functions/triggers/onUploadsInsert.js
+++ functions/triggers/onUploadsInsert.js
@@ -1,10 +1,8 @@
-import trigger from '../../functions-util/trigger'
-
-export default trigger(async (req, res) => {
+export default async (req, res) => {
   // See event payload format:
   // https://hasura.io/docs/latest/graphql/core/event-triggers/payload.html
   console.log('JSON.stringify')
   console.log(JSON.stringify(req.body, null, 2))
 
   return res.send('OK')
-})
+}
