d1fe7e2277f69292d7249ba0461c8a82d672d790
Jerry Jäppinen <eiskis@gmail.com>
2022-03-21 09:54:17 +0000

    Logging

--- functions/triggers/onUploadsInsert.js
+++ functions/triggers/onUploadsInsert.js
@@ -3,8 +3,8 @@ import trigger from '../../functions-util/trigger'
 export default trigger(async (req, res) => {
   // See event payload format:
   // https://hasura.io/docs/latest/graphql/core/event-triggers/payload.html
-  console.log('JSON.stringify')
-  console.log(JSON.stringify(req.body, null, 2))
+  // eslint-disable-next-line no-console
+  console.log('onUploadsInsert', JSON.stringify(req.body, null, 2))
 
   return res.send('OK')
 })
