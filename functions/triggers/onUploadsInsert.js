d1d579ce0c6fa41f3f85dffaf3de0c272a1478ba
Jerry Jäppinen <eiskis@gmail.com>
2022-03-21 16:37:05 +0000

    Logging

--- functions/triggers/onUploadsInsert.js
+++ functions/triggers/onUploadsInsert.js
@@ -22,7 +22,7 @@ export default trigger(async (req, res) => {
   // See event payload format:
   // https://hasura.io/docs/latest/graphql/core/event-triggers/payload.html
   // eslint-disable-next-line no-console
-  console.log('onUploadsInsert', `(${upload.id})`)
+  console.log('onUploadsInsert', process.env.NHOST_ADMIN_SECRET, `(${upload.id})`)
 
   // Writes a log entry into logs table
   // In a real app, you could use this approach for business analytics
